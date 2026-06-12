06be4f188ab8d2054ecc1917c49166ad37a0f329
MOHBDE <bdir.1213@gmail.com>
2026-06-12 13:29:26 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    - Statiska resurser (React, Babel, highlight.js, typsnitt): cache-first
    - Supabase (data + bilder): alltid nätverk, aldrig cache
 */
-const CACHE = 'scada-kb-v16';
+const CACHE = 'scada-kb-v17';
 const MEDIA = 'scada-media';
 const ASSETS = [
   './',
